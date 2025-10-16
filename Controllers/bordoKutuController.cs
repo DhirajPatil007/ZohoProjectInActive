3ffe825b5b053ae4616fa64b816edc7980c4d042
DhirajPatil007 <dhiraj@xlpxl.com>
2025-10-16 10:43:09 +0530

    boordo

---
+++ Controllers/bordoKutuController.cs
@@ -0,0 +1,25 @@
+using System;
+
+using System.Data;
+using System.Data.SqlClient;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Newtonsoft.Json.Converters;
+using Newtonsoft.Json;
+
+
+
+namespace FretAPI.Controllers
+{
+
+    [Route("api/[controller]")]
+
+    public class BordokutuContoller : Controller
+    {
+
+    }
+}
+
